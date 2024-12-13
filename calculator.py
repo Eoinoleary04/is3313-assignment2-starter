d7d48b54fd313d7d1960170648fcd73d189d5038
Eoinoleary04 <122485184@umail.ucc.ie>
2024-12-13 13:43:14 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -25,6 +25,6 @@ class Calculator:
 
     def modulus(self, a, b):
         """Calculates the modulus of two numbers and returns the result."""
-        def multiply(self, base):
-    return self * base
+    def multiply(self, base):
+        return self * base
 
